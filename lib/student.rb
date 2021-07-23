c0544333a039a22727e7d2247d2e74c966f2250a
JoeMilius <joemilius@gmail.com>
2021-07-23 14:21:51 -0600

    Completed Assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,6 @@
 class Student
 
+
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
@@ -14,6 +15,7 @@ class Student
   end
   
   def self.create_table
+    
     sql =  <<-SQL 
       CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY, 
